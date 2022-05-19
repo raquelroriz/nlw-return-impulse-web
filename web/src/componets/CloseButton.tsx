539f8299b86490af88fd954a8013c4318c1b032f
Maria Raquel <maria.raquel.roriz@gmail.com>
2022-05-19 10:35:16 -0300

    fix: correção de typo

--- web/src/componets/CloseButton.tsx
+++ web/src/componets/CloseButton.tsx
@@ -7,7 +7,7 @@ export function CloseButton() {
       className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100"
       title="Fechar formulário de feedback"
     >
-      <X wieght="bold" className="w-4 h-4" />
+      <X weight="bold" className="w-4 h-4" />
     </Popover.Button>
   );
 }
